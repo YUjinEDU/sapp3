91c6affb6d78e5b0b05e31eb7d90e7e4c8d5466d
깃허브-YUjinEDU <20191906@edu.hanbat.ac.kr>
2024-07-15 14:43:28 +0900

    1message print on web_1day

--- src/main/java/com/ll/sapp/MainController.java
+++ src/main/java/com/ll/sapp/MainController.java
@@ -2,11 +2,13 @@ package com.ll.sapp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
     @GetMapping("/sapp")
-    public void index() {
-        System.out.println("index");
+    @ResponseBody
+    public String index() {
+        return "안녕하세요";
     }
 }
