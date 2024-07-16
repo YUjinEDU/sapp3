ab37f8e76f47008bbba5c9bd46843c914f276d2e
깃허브-YUjinEDU <20191906@edu.hanbat.ac.kr>
2024-07-16 13:21:45 +0900

    SBB 1ãst

--- src/main/java/com/ll/sapp/MainController.java
+++ src/main/java/com/ll/sapp/MainController.java
@@ -9,6 +9,11 @@ public class MainController {
     @GetMapping("/sapp")
     @ResponseBody
     public String index() {
-        return "안녕하세요";
+        return "안녕하세요 스프링부트 페이지입니다.";
+    }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
     }
 }
--- src/test/java/com/ll/sapp/SappApplicationTests.java
+++ src/test/java/com/ll/sapp/SappApplicationTests.java
@@ -1,13 +1,40 @@
 package com.ll.sapp;
 
+import com.ll.sapp.answer.Answer;
+import com.ll.sapp.answer.AnswerRepository;
+import com.ll.sapp.question.Question;
+import com.ll.sapp.question.QuestionRepository;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest
-class SappApplicationTests {
+class SbbApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
+
+
+	@Autowired
+	private AnswerRepository answerRepository;
+
+
+	@Transactional
 	@Test
-	void contextLoads() {
-	}
+	void testJpa() {
+		Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
 
+		List<Answer> answerList = q.getAnswerList();
+
+		assertEquals(1, answerList.size());
+		assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+	}
 }
